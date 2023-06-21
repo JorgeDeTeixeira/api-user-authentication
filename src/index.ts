44b5bcc2c59fc60ace0e13a1c5912299d3db3bce
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2023-06-21 14:32:27 -0300

    Criando um server com express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello World!")
+import express, { Request, Response, NextFunction } from "express";
+const app = express();
+const PORT = 3000;
+
+app.get("/status", (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ foo: "bar" });
+});
+
+app.listen(PORT, () => console.log("Server is running in port 3000."));
