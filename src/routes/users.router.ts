529b2dacbd272b722242c2c02114df9045dddada
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2023-06-21 15:29:28 -0300

    Implementanto rota por id de usuário

--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -8,4 +8,13 @@ userRouter.get("/users", (req: Request, res: Response, next: NextFunction) => {
     res.status(200).json(users);
 });
 
+userRouter.get(
+    "/users:uuid",
+    (req: Request<{ uuid: String }>, res: Response, next: NextFunction) => {
+        const uuid = req.params.uuid;
+
+        res.sendStatus(200);
+    }
+);
+
 export default userRouter;
